d452a8f5e41889f71f18f08634ca71b5d0ba2184
Jaivignesh P G <your-email@example.com>
2026-07-10 15:03:04 +0530

    Update certificates

--- src/components/Certificates.tsx
+++ src/components/Certificates.tsx
@@ -2,87 +2,87 @@ function Certificates() {
   const certificates = [
     {
       title: "Introduction to Large Language Models",
-      file: "/Certificate/Introduction To Large Language Models.jpeg",
+      file: `${import.meta.env.BASE_URL}Certificate/Introduction To Large Language Models.jpeg`,
     },
     {
       title: "Introduction to AI",
-      file: "/Certificate/Introduction To AI.jpeg",
+      file: `${import.meta.env.BASE_URL}Certificate/Introduction To AI.jpeg`,
     },
     {
       title: "Introduction to Generative AI",
-      file: "/Certificate/Introduction To Generative AI.jpeg",
+      file: `${import.meta.env.BASE_URL}Certificate/Introduction To Generative AI.jpeg`,
     },
     {
       title: "Introduction to Responsible AI",
-      file: "/Certificate/Introduction To Responsible AI.jpeg",
+      file: `${import.meta.env.BASE_URL}Certificate/Introduction To Responsible AI.jpeg`,
     },
     {
       title: "AI For Everyone",
-      file: "/Certificate/AI For Everyone.jpeg",
+      file: `${import.meta.env.BASE_URL}Certificate/AI For Everyone.jpeg`,
     },
     {
       title: "Software Engineering in AI",
-      file: "/Certificate/Software Engineering in Ai.jpeg",
+      file: `${import.meta.env.BASE_URL}Certificate/Software Engineering in Ai.jpeg`,
     },
     {
       title: "Develop Generative AI Applications",
-      file: "/Certificate/Develop Generative AI Applications.jpeg",
+      file: `${import.meta.env.BASE_URL}Certificate/Develop Generative AI Applications.jpeg`,
     },
     {
       title: "Generative AI and LLMs: Architecture and Data Preparation",
-      file: "/Certificate/Generative AI and LLMs Architecture and Data Preparation.jpeg",
+      file: `${import.meta.env.BASE_URL}Certificate/Generative AI and LLMs Architecture and Data Preparation.jpeg`,
     },
     {
       title: "Database Structures and Management with MySQL",
-      file: "/Certificate/Data structure and management with mysql.jpeg",
+      file: `${import.meta.env.BASE_URL}Certificate/Data structure and management with mysql.jpeg`,
     },
     {
       title: "Discover The Art Of Prompting",
-      file: "/Certificate/Discover The Art Of Prompting.jpeg",
+      file: `${import.meta.env.BASE_URL}Certificate/Discover The Art Of Prompting.jpeg`,
     },
     {
       title: "Use AI Responsibly",
-      file: "/Certificate/Use AI Responsibly.jpeg",
+      file: `${import.meta.env.BASE_URL}Certificate/Use AI Responsibly.jpeg`,
     },
     {
       title: "Maximize Productivity With AI Tools",
-      file: "/Certificate/Maximize Productivity With AI Tools.jpeg",
+      file: `${import.meta.env.BASE_URL}Certificate/Maximize Productivity With AI Tools.jpeg`,
     },
     {
       title: "Responsible AI: Applying AI Principles with Google Cloud",
-      file: "/Certificate/Responsible AI Applying AI Principles with Google Cloud.jpeg",
+      file: `${import.meta.env.BASE_URL}Certificate/Responsible AI Applying AI Principles with Google Cloud.jpeg`,
     },
     {
       title: "Basic Data Descriptors, Statistical Distributions",
-      file: "/Certificate/Basic Data Descriptors, Statistical Distributions and Application to Business Decisions.jpeg",
+      file: `${import.meta.env.BASE_URL}Certificate/Basic Data Descriptors, Statistical Distributions, and Application to Business Decisions.jpeg`,
     },
     {
       title: "Optimize Deep Learning Models for Peak AI",
-      file: "/Certificate/Optimize Deep Learning models for Peak AI.jpeg",
+      file: `${import.meta.env.BASE_URL}Certificate/Optimize Deep Learning models for Peak AI.jpeg`,
     },
     {
       title: "Design And Build Custom Neural Networks",
-      file: "/Certificate/Design And Build Custom Neural Networks.jpeg",
+      file: `${import.meta.env.BASE_URL}Certificate/Design And Build Custom Neural Network.jpeg`,
     },
     {
       title: "Merge Sort",
-      file: "/Certificate/Merge Sort.jpeg",
+      file: `${import.meta.env.BASE_URL}Certificate/Merge Sort.jpeg`,
     },
     {
       title: "Getting Started With Artificial Intelligence",
-      file: "/Certificate/Getting_Started_With_Artificial_Intelligence.pdf",
+      file: `${import.meta.env.BASE_URL}Certificate/Getting_Started_With_Artificial_Intelligence_Badge.png`,
     },
     {
       title: "NOOB HACKFEST 2026",
-      file: "/Certificate/NOOB HACKFEST.jpeg",
+      file: `${import.meta.env.BASE_URL}Certificate/NOOB HACKFEST.jpeg`,
     },
     {
       title: "BIT V-PRAYUKTI '25",
-      file: "/Certificate/BIT V-PRAYUKTI 25.pdf",
+      file: `${import.meta.env.BASE_URL}Certificate/BIT V-PRAYUKTI’ 25.pdf`,
     },
     {
       title: "NMBA Certificate",
-      file: "/Certificate/nmba_certificate.pdf",
+      file: `${import.meta.env.BASE_URL}Certificate/nmba_certificate.pdf`,
     },
   ];
 
